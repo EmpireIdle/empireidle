97bd9df6fe9e0edc2eb0d59bb06dfb4507c3ef7b
EmpireIdle <team.empireidle@gmail.com>
2025-12-02 15:50:35 +0000

    Update2 game.js

--- game.js
+++ game.js
@@ -813,3 +813,18 @@
         updateUI();
         setInterval(() => productionTick(1), 1000);
     })();
+
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(app);
+const auth = firebase.auth();
